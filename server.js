9dbc43203f4e75a11c46d21fe1c4d3ef2d029e2a
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-02-24 04:20:23 +0000

    mongoose

--- server.js
+++ server.js
@@ -10,19 +10,18 @@ var mongo = require('mongodb').MongoClient;
 
 var app = express();
 require('dotenv').load();
-    mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
-    if (err) {
-        throw new Error('Database failed to connect!');
-    } else {
-        console.log('MongoDB successfully connected on port 27017.');
-    }
-
-routes(app);
-create(app, db);
+mongoose.connect('mongodb://localhost:27017/clementinejs');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  routes(app);
+  create(app, db);
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
-});
+
 });
