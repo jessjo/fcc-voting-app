411ccfddc42cf97c890e4cbbda0777468badeb34
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-03-04 00:38:54 +0000

    changing Poll format to support creator and voters

--- app/models/polls.js
+++ app/models/polls.js
@@ -7,7 +7,9 @@ var Schema = mongoose.Schema;
 var Poll = new Schema({
     question: String,
     choices: [{ category: String, votes: Number }],
-    id: Number
+    id: Number,
+    creator: String,
+    voters: [String]
 });
 
 module.exports = mongoose.model('Poll', Poll);
